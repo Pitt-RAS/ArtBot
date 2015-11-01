5432cf573c30ec793fd7fa9380fc30483d7042b0
Alexander Pickering <alexandermpickering@gmail.com>
2015-11-01 15:44:07 +0000

    Created concurrent backend class with example code

---
+++ backend.py
@@ -0,0 +1,77 @@
+
+
+class Backend:
+
+    togui = None
+    toback = None
+
+    #Calling methods in backend should be done by pusshing the method name and any arguments it needs into it's queue as a tuple
+    #If the method returns anything, be sure to take it out of the queue
+    def __init__ (self, toguiqueue, tobackendqueue):
+        self.togui = toguiqueue
+        self.toback = tobackendqueue
+        while ( True ):
+            method = tobackendqueue.get()
+            if ( hasattr (self, method ) ):
+                call = getattr(self, method)
+                try:
+                    call()
+                except AttributeError:
+                    print("Unable to find method \"" + method + "\" in backend.py")
+                    raise AttributeError #Raise the error anyway so we get traceback
+            else:
+                print("Backend.py does not have method\"" + method + "\"")
+
+    def dosomething(self):
+        print("Doing something")
+
+    def getsomething(self):
+        print("Pushing \"1\" to queue")
+        self.togui.put(1)
+
+    def getsomethingwithargs(self):
+        one = self.toback.get(block=True)
+        two = self.toback.get(block=True)
+        self.togui.put(one*two)
+
+    def operationthattakesawhile(self):
+        time.sleep(5)
+        self.togui.put(1)
+
+
+   #Instead of keeping a local database, interface with find.pitt.edu 
+
+def startbackend(togui, toback):
+    c = Backend(togui, toback)
+
+#Code to test this class
+if __name__ == '__main__':
+    import queue
+    import threading
+    import time
+
+    togui = queue.Queue()
+    toback = queue.Queue()
+
+    t = threading.Thread(target=startbackend,args=(togui,toback))
+    t.start()
+    
+    toback.put("dosomething")
+
+    toback.put("getsomething")
+    num = togui.get(block=True)
+    print("Got:" + str(num))
+
+    toback.put("getsomethingwithargs")
+    toback.put(4)
+    toback.put(5)
+    mul = togui.get(block=True)
+    print("Got:" + str(mul))
+
+    toback.put("operationthattakesawhile")
+    val = None
+    while(val == None and togui.empty()):
+       val = togui.get()
+    print("Got:" + str(val))
+
+
--- start.py
+++ start.py
@@ -12,6 +12,8 @@ modules = {}
 modules['sys'] = ['Will not be able to run','Every python interpreter should have this']
 modules['os'] = ['Will not be able to run','Every python interpreter should have this'] 
 modules['sqlite3'] = ['Will not be able to run','Install the sqlite3 package for python3']
+modules['queue'] = ['Will not be able to run', 'Every python interpreter should have his']
+modules['threading'] = ['Will not be able to run','Every python interpreter should have this']
 
 #Non-standard
 modules['PyQt5.QtGui'] = ['Will not be able to display','Install PyQt5 for python3(pip install pyqt5)']
@@ -22,14 +24,28 @@ modules['PyQt5.QtWidgets'] = ['Will not be able to display','Install PyQt5 for p
 modules['mainwindow.py'] = ['Will be unable to start','Make sure you have all the code, and it is in the correct directory']
 
 #Load all the modules we need
+failed = False
 for mod in modules:
     try:
         importlib.import_module(mod)
     except:
+        failed = True
         print("Failed to load module:",mod)
         print(modules[mod][0])
         print(modules[mod][1])
 
-#Start the backend on a different thread than the display.
-#Won't actually run faster due to GIL, but makes it easy to decouple graphics and processing
+#Don't continue if we failed to load something
+if(failed):
+    exit()
+
+#Start the backend on a different thread than the display, in order to keep the display responsive even while we are doing processor intensive stuff
+#Won't actually run faster, but makes it easy to decouple graphics and processing
+
+#Create a pair of queues so that the graphics and gui threads can communicate
+togui = queue.Queue()
+tobackend = queue.Queue()
+
+def startgui():
+    pass
+
 
