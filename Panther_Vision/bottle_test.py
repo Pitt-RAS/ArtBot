0ecd98e82f056b417378fb1c18c60c9414ac79ce
jlynchpitt <jtl44@pitt.edu>
2016-04-10 19:02:30 -0400

    Fixed refresh issue with starting of panther vision

--- Panther_Vision/bottle_test.py
+++ Panther_Vision/bottle_test.py
@@ -1,5 +1,8 @@
 from bottle import route, run, static_file
-from face_video import panther_video
+from face_video import panther_video, kill_video
+from selenium import webdriver
+from threading import Thread
+import thread
 
 #return variable??? parse file names into HTML?
 
@@ -7,13 +10,39 @@ from face_video import panther_video
 def hello():
     return "Hello World!"
 	
-@route('/camera')
+@route('/NO/camera')
 def camera():
-	panther_video()
-	return "Success"
+	#panther_video()
+	try:
+		thread.start_new_thread ( panther_video, ())
+	except:
+		print ("Error: Did not start thread")
+	try:	
+		thread.start_new_thread ( kill_video, ())
+	except:
+		print("Error: kill thread did not start")
+		
+	return static_file("Panther_Kiosk_Panther_Vision_Menu.html", root='../')	
 
 @route('/static/<filename:path>')
 def send_static(filename):
-    return static_file(filename, root='./')	
+	if (filename == "Panther_Kiosk_Panther_Vision_Menu.html"):
+		#print("if")
+		t = thread.start_new_thread ( panther_video, ())
+		#t = Thread(target=panther_video, args=())
+		#t.start()
+		#t.exit()
+		#t2 = Thread(target=panther_video, args=())
+		#t2.start()
+		#driver = webdriver.Firefox()
+		#driver.get("http://localhost:8080/NO/camera")
+		#driver.refresh()
+		
+		#t.exit()
+		#t2 = thread.start_new_thread ( panther_video, ())
+		return static_file("Panther_Kiosk_Panther_Vision_Menu.html", root='../')
+	else:
+		print("else")
+		return static_file(filename, root='../')	
 	
 run(host='localhost', port=8080, debug=True)
