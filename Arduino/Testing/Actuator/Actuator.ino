c783932153ca34e8c0d60c67e8976ff986649d09
JKenneson <jmk213@pitt.edu>
2016-09-27 21:48:05 -0400

    Linear Actuator send to specific position
    Functions written to get the current position (0-100% extended) of the
    linear actuator, as well as sending the actuator to that specific
    position.

--- Arduino/Testing/Actuator/Actuator.ino
+++ Arduino/Testing/Actuator/Actuator.ino
@@ -12,30 +12,67 @@ void setup() {
   pinMode(potPin, INPUT);
 }
 
+//This loop simply tests functions below
 void loop() {
-  //Read the potentiometer
+  //Send to 1/4, 1/2, 3/4, and full
+  sendToPosition(25);
+  delay(1000);
+  sendToPosition(50);
+  delay(1000);
+  sendToPosition(75);
+  delay(1000);
+  sendToPosition(100);
+  delay(1000);
+}
+
+/*  @Author: Jon Kenneson
+ *  @Date: September 2016
+ *  
+ *  This function reads the potentiometer and returns mapped value in range 0-100% 
+ */
+int getCurrentPosition() {
   int sensorValue = analogRead(potPin);
-  Serial.println(sensorValue);//prints value to serial port
-  delay(50);        // delay in between reads for stability
+  sensorValue = map(sensorValue, 0, 900, 0, 100);
+
+  return sensorValue;
 }
 
-void extendActuator(int actuator) {
-  //Set one relay one and the other off
-  //this will move extend the actuator
-  digitalWrite(In1, HIGH);
-  digitalWrite(In2, LOW);
+/*  @Author: Jon Kenneson
+ *  @Date: September 2016
+ *  
+ *  This function sends the actuator to a certain position determined by percentage extended sent in 
+ *  The actuator will extend if it is below the intended position, retract if above, or stay still if already in place
+ */
+void sendToPosition(int position) {
+  int currentPosition = getCurrentPosition();
+
+  //If we're below, extend the actuator
+  if(currentPosition < position) {
+    while(currentPosition < position) {
+      digitalWrite(In1, HIGH);
+      digitalWrite(In2, LOW);
+      currentPosition = getCurrentPosition();
+    }
+      //Stop motion
+      digitalWrite(In1, LOW);
+      digitalWrite(In2, LOW);
+  }
+  //If we're above, retract the actuator
+  else if (currentPosition > position) {
+     while(currentPosition > position) {
+      digitalWrite(In1, LOW);
+      digitalWrite(In2, HIGH);
+      currentPosition = getCurrentPosition();
+    }
+    //Stop motion
+    digitalWrite(In1, LOW);
+    digitalWrite(In2, LOW);
+  }
+  //Otherwise, send both signals low to hold position there
+  else {
+    //Stop motion
+    digitalWrite(In1, LOW);
+    digitalWrite(In2, LOW);
+  }
 }
 
-void retractActuator(int actuator) { 
-  //Set one relay off and the other on 
-  //this will move retract the actuator 
-  digitalWrite(In1, LOW);
-  digitalWrite(In2, HIGH); 
-}
-
-void stopActuator(int actuator) {
- //Set both relays off
- //this will stop the actuator in a braking
- digitalWrite(In1, LOW);
- digitalWrite(In2, LOW); 
-} 
