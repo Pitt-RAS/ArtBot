077a345a6e0b0f6f939ddbbe22c2da413500299f
jlynchpitt <joelynch523@gmail.com>
2017-01-24 21:34:09 -0500

    Possibly fixed cache issue

--- User_Interface/Website/Panther_Vision/bottle_test.py
+++ User_Interface/Website/Panther_Vision/bottle_test.py
@@ -4,49 +4,17 @@ from selenium import webdriver
 from threading import Thread
 import thread
 
-#return variable??? parse file names into HTML?
-
 @route('/hello')
 def hello():
     return "Hello World!"
 	
-@route('/NO/camera')
-def camera():
-	#panther_video()
-	try:
-		thread.start_new_thread ( panther_video, ())
-	except:
-		print ("Error: Did not start thread")
-	try:	
-		thread.start_new_thread ( kill_video, ())
-	except:
-		print("Error: kill thread did not start")
-		
-	return static_file("Panther_Kiosk_Panther_Vision_Menu.html", root='../')	
-
 @route('/static/<filename:path>')
 def send_static(filename):
+	print("Getting file" + filename)
 	if (filename == "Panther_Kiosk_Panther_Vision_Menu.html"):
-		#print("if")
 		t = thread.start_new_thread ( panther_video, ())
-		#t = Thread(target=panther_video, args=())
-		#t.start()
-		#t.exit()
-		#t2 = Thread(target=panther_video, args=())
-		#t2.start()
-		#driver = webdriver.Firefox()
-		#driver.get("http://localhost:8080/NO/camera")
-		#driver.refresh()
-		
-		#t.exit()
-		#t2 = thread.start_new_thread ( panther_video, ())
-		return static_file("Panther_Kiosk_Panther_Vision_Menu.html", root='../')
 	elif(filename == "Panther_Kiosk_Main.html"):
-		print("in bottle")
 		t2 = thread.start_new_thread(kill_video, ())
-		return static_file("Panther_Kiosk_Main.html", root='../')
-	else:
-		print("else")
-		return static_file(filename, root='../')	
+	return static_file(filename, root='../')	
 	
 run(host='localhost', port=8081, debug=True)
