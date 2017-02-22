2dfbec920294fba861074ca1bbdd686ac2a28858
Woodrow Fulmer <neomagam@gmail.com>
2017-02-21 21:45:32 -0500

    Working oneArmExample and hotfixes

--- Arduino/Custom_Libraries/Arm/Arm/examples/oneArmExample/oneArmExample.ino
+++ Arduino/Custom_Libraries/Arm/Arm/examples/oneArmExample/oneArmExample.ino
@@ -3,18 +3,27 @@
 Arm myArm(9, 10, A2, 2, 3, A3);
 
 bool retract;
+bool moving;
 
 void setup() {
+  moving = false;
   retract = true;
   myArm.servo(44);
 }
 
 void loop() {
-  if(!myArm.move())
+  moving = myArm.move();
+  if(!moving)
   {
     if(retract)
+    {
       myArm.setMoveType(0);
+      retract = false;
+    }
     else
+    {
       myArm.setMoveType(3);
+      retract = true;
+    }
   }
 }
